4770721d12b5c50c46a2512d4a06094865243a87
bansalsurya <2647surya@gmail.com>
2020-05-29 00:22:30 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent';
+function App() {
+  return (
+    <div>
+      <Navbar dark color='primary'>
+        <div className='container'>
+          <NavbarBrand href='/'>Ristornte Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
